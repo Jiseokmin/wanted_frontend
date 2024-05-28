e4bb5f2e26e48cc97c28e2399be394595da60c71
jiseokmin <37jr@naver.com>
2024-05-28 22:13:23 +0900

    feat:image rotate

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [message, setMessage] = useState("")   
+  const [rotation, setRotation] = useState({ rotationX: 0, rotationY: 0 });
+
+  const handleMouseMove = (event) => {
+    const mouseX = event.nativeEvent.offsetX;
+    const mouseY = event.nativeEvent.offsetY;
+    const rotationX = 4/30 * mouseY - 20;
+    const rotationY = -1/5 * mouseX + 20;
+    
+    setRotation({rotationX, rotationY});
+  };
+
 
   return (
     <div className="App">
-      악인전
+      <header className="App-header">
+        <p>악인전</p>
+        <div className="appImage" 
+             onMouseMove={handleMouseMove}
+             style={{
+               transform: `perspective(1000px) rotateX(${rotation.rotationX}deg) rotateY(${rotation.rotationY}deg)`
+             }}>
+          </div>
+      </header>
     </div>
   );
 }
