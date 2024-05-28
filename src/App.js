0dddb21063db0e42b11de45ddab48f75c40f8a4a
jiseokmin <37jr@naver.com>
2024-05-28 23:43:56 +0900

    feat:add design

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [rotation, setRotation] = useState({ rotationX: 0, rotationY: 0 });
+  const [backgroundPosition, setBackgroundPosition] = useState({ x: 0, y: 0});
+
 
   const handleMouseMove = (event) => {
     const mouseX = event.nativeEvent.offsetX;
     const mouseY = event.nativeEvent.offsetY;
     const rotationX = 4/30 * mouseY - 20;
     const rotationY = -1/5 * mouseX + 20;
+
+    const backgroundPositionX = mouseX / 5;
+    const backgroundPositionY = mouseY / 5;
     
     setRotation({rotationX, rotationY});
+    setBackgroundPosition({ backgroundPositionX, backgroundPositionY});
   };
 
 
@@ -19,12 +25,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>악인전</p>
-        <div className="appImage" 
-             onMouseMove={handleMouseMove}
-             style={{
-               transform: `perspective(1000px) rotateX(${rotation.rotationX}deg) rotateY(${rotation.rotationY}deg)`
-             }}>
+        <div className="cardContainer" onMouseMove={handleMouseMove}
+              style={{
+                transform: `perspective(1000px) rotateX(${rotation.rotationX}deg) rotateY(${rotation.rotationY}deg)`
+              }}>
+          <div className="overlay" onMouseMove={handleMouseMove} 
+               style={{
+                backgroundPosition: `${backgroundPosition.backgroundPositionX}px ${backgroundPosition.backgroundPositionY}px`
+              }}>
           </div>
+          <div className="card" ></div>
+        </div>
       </header>
     </div>
   );
