554659db7b4b0c9edfa324a8bcfe9853a0014ab6
lzeroj <dladudwns366@naver.com>
2024-06-08 09:47:17 +0900

    임영준 커밋

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 import { useEffect, useState } from 'react';
+import Login from './Login';
 
 function App() {
-  const [message, setMessage] = useState("")   
+  // const [message, setMessage] = useState("")   
 
-  useEffect(()=>{
-      fetch("/test")      
-        .then(res => res.text())      
-        .then(m=>setMessage(m))  
-      }, [])
+  // useEffect(()=>{
+  //     fetch("/test")      
+  //       .then(res => res.text())      
+  //       .then(m=>setMessage(m))  
+  //     }, [])
 
+  // return (
+  //   <div>
+  //         {message}
+  //   </div>
+  // );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {message}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div><Login /></div>
+  )
 }
 
 export default App;
